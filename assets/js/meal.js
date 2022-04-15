d55fddd5408777e4cecd13ab50f62f86606b505d
Angel Santos <iiisantosjangel@gmail.com>
2022-04-15 16:55:58 -0700

    added meal.js

---
+++ assets/js/meal.js
@@ -0,0 +1,8 @@
+function searchMeal(){
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood&a=Canadian/images/media/meals/llcbn01574260722.jpg/preview')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+          displayInfo(data);
+        })}; 
+      searchMeal();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const myElement = document.getElementById("")
-var redirectHomepage = function() {
-    location.replace("./homepage.html");
-}
+ var redirectHomepage = function() {
+     location.replace("./homepage.html");
+ }
 
-setInterval(redirectHomepage, 5000);
+}
+ setInterval(redirectHomepage, 5000);
