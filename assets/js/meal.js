a9686edd4c05cc261fb26b01f86c02f5cfabf417
Rush <kriscookies20@gmail.com>
2022-04-16 16:51:22 -0700

    meal ingredients displayed

--- assets/js/meal.js
+++ assets/js/meal.js
@@ -1,6 +1,3 @@
-// const mealImg = document.getElementById('meal');
-// const mealDetails = document.getElementById('ingredients');
-
 
 var searchMeal = function (){
   
@@ -17,19 +14,40 @@ var searchMeal = function (){
       }); 
     }
 
+    // search meal info
+  var getMealIngredients = function() {
 
+    var apiUrlIng = 'https://www.themealdb.com/api/json/v1/1/lookup.php';
+    fetch(apiUrlIng).then(function(response) {
+      if (response.ok) {
+        return response.json().then(function(data) {
+          displayMealInfo(data);
+          console.log("apiUrlIng")
+        });
+      };
+    });
+  }
+
+      // diplaying meal picture
       var displayMealInfo = function(data) {
         var meal = data.meals[0].strMealThumb;
-        console.log(meal);
+        var mealIngr = data.meals[0].strInstructions;
+        var mealVideo = data.meals[0].strYoutube;
+        console.log(mealIngr);
 
         // display the image of the meal
         $("#mealimg").attr("src", meal);
-      }
 
-      
+        // display ingredients
+        $("#ingredients").text(mealIngr);
+
+        // youTube video link
+        $("#meal-video").attr("href", strYoutube);
+      }
 
   
 //displayMealInfo();
+      getMealIngredients();
       searchMeal();
-      
+
 // var apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood&a=Canadian/images/media/meals/llcbn01574260722.jpg/preview';  
