76f8bfb36409f4ca00c2583e45627d05540cf223
Rush <kriscookies20@gmail.com>
2022-04-16 10:41:33 -0700

    working on displaying meal img

--- assets/js/meal.js
+++ assets/js/meal.js
@@ -1,8 +1,29 @@
-function searchMeal(){
-    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood&a=Canadian/images/media/meals/llcbn01574260722.jpg/preview')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+// const mealImg = document.getElementById('meal');
+// const mealDetails = document.getElementById('ingredients');
+
+
+var searchMeal = function (){
+  var apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood&a=Canadian/images/media/meals/llcbn01574260722.jpg/preview';
+    // search meal
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        return response.json().then(function(data) {
           displayInfo(data);
-        })}; 
+        });
+        };
+      }); 
+    }
+
+
+      var displayMealInfo = function(data) {
+        var meal = data.meals[0].strMeal;
+
+        // display the image of the meal
+        $("#mealimg").attr("src", meal.meals);
+      }
+
+      
+
+    
+      // displayMealInfo();
       searchMeal();
